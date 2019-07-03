16596ee63a5a70bd4af89b3fc8ef3e886de7c053
LAPTOP-A22IJGIL\lakshman <lakshman.scrum@gmail.com>
2019-07-03 20:18:13 +0200

    Initial commit

---
+++ src/main/java/com/learncamel/file/CopyFilesMultiRoute.java
@@ -0,0 +1,41 @@
+package com.learncamel.file;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * Created by z001qgd on 6/7/17.
+ */
+public class CopyFilesMultiRoute {
+
+    public static void main(String[] args) {
+        CamelContext context= new DefaultCamelContext();
+
+        try{
+            context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("file:data/input?noop=true")
+                            //.to("log:?level=INFO&showBody=true&showHeaders=true")
+                            .log("Received Message is ${body} and Headers are ${headers}")
+                            .to("file:data/output")
+                            .to("file:data/anothetdestination");
+
+                    from("file:data/input1?noop=true")
+                            .to("file:data/nextoutput");
+
+
+                }
+            });
+
+            context.start();
+            Thread.sleep(5000);
+            context.stop();
+
+        }catch(Exception e){
+            System.out.println("Inside Exception : " + e);
+        }
+
+    }
+}
---
+++ src/test/java/com/learncamel/direct/SampleDirectRouteTest.java
@@ -0,0 +1,53 @@
+package com.learncamel.direct;
+
+import com.learncamel.file.CopyFilesRoute;
+import org.apache.camel.Exchange;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Created by z001qgd on 6/8/17.
+ */
+public class SampleDirectRouteTest extends CamelTestSupport {
+    @Override
+    public RoutesBuilder createRouteBuilder() throws Exception {
+        return new SampleDirectRoute();
+    }
+
+
+    @Test
+    public void sampleRouteTest() throws InterruptedException {
+
+        /**
+         * Producer Template.
+         */
+        template.sendBody("direct:sampleInput","Hello" );
+
+        Thread.sleep(5000);
+        
+        File file = new File("sampleOutput");
+
+        assertTrue(file.isDirectory());
+
+        /**
+         * Consumer Template.
+         */
+        Exchange exchange = consumer.receive("file:sampleOutput");
+
+        System.out.println("Received body is :" + exchange.getIn().getBody());
+        System.out.println("File Name is :" + exchange.getIn().getHeader("CamelFileName"));
+
+        assertEquals("output.txt", exchange.getIn().getHeader("CamelFileName"));
+
+        
+
+    }
+
+
+
+
+}
